56a0722990c01a89dbc0dc145b87909daed46347
NisalaRV <nisalaravihara129@gmail.com>
2023-11-09 23:53:50 +0530

    responsive

--- src/main.js
+++ src/main.js
@@ -29,3 +29,17 @@ let navbar = document.querySelector('.navbar');
 menu.onclick = () =>{
     navbar.classList.toggle('active')
 }
+
+// =======================
+let darkmode = document.querySelector('#darkmode');
+
+darkmode.onclick = () =>{
+    if (darkmode.classList.contains('bx-moon')){
+       darkmode.classList.replace('bx-moon', 'bx-sun');
+       document.body.classList.add('active');
+    }
+    else {
+        darkmode.classList.replace('bx-sun', 'bx-moon');
+        document.body.classList.remove('active');
+    }
+}
